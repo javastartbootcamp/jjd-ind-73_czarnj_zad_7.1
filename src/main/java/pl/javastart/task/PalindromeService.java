bec2a700afe2ac09f66a2e17a1e4e1c43183adeb
Julka Czarnasiak <czarna13500@gmail.com>
2023-07-16 09:23:28 +0200

    Reduce amount of checks

--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -8,7 +8,7 @@ public class PalindromeService {
             return false;
         }
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             if (array[i] != array[array.length - 1 - i]) {
                 return false;
             }
